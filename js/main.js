2d7bdd033594a533cdcfb684e5b7a00e40ab8052
Dmitriy-Tkachenko <yeschkaa@gmail.com>
2021-07-06 03:18:32 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 $(document).ready(function() {
     $('#toggle-theme').change(function() {
+        var link = document.getElementById("theme-link");
+        let lightTheme = "css/light.css";
+        let darkTheme = "css/dark.css";
+        var currTheme = link.getAttribute("href");
+        var theme = "";
         if(this.checked==true) {
-            document.body.classList.add('dark-theme')
+            currTheme = darkTheme;
+            theme = "dark";
+            document.querySelectorAll('.task__edit-img').forEach(function(item) {
+                item.src = "img/pen-white.png";
+            })
+            document.querySelectorAll('.task__add-img').forEach(function(item) {
+                item.src = "img/confirm-white.png";
+            })
         } else {
-            document.body.classList.remove('dark-theme')
+            currTheme = lightTheme;
+            theme = "light";
+            document.querySelectorAll('.task__edit-img').forEach(function(item) {
+                item.src = "img/pen.png";
+            })
+            document.querySelectorAll('.task__add-img').forEach(function(item) {
+                item.src = "img/confirm.png";
+            })
         }
+
+        link.setAttribute("href", currTheme);
+        Save(theme);
     });
 });
